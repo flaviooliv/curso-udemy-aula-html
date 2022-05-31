8e3c008fac7eea630a257f8b6dd725389ecb9067
flaviooliv <flaviooliveiraalice@hotmail.com>
2022-05-31 15:26:19 -0300

    comentarios

---
+++ javascript/aula3/aula4/comentarios.js
@@ -0,0 +1,13 @@
+// Escrever um comentario
+console.log('ola mundo'); // Aqui tem outro comentario
+console.log('linha 1');
+// comentário
+
+/* 
+Um bloco de código
+linha de comentario.
+*/
+console.log('linha 2');
+console.log('linha 3');
+console.log('linha 4');
+// vai ser ignorado pela indine 

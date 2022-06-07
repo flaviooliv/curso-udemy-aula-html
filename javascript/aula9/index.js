0e1637ee1c7fb3f508022a76c419eb12f354d0c0
flaviooliv <flaviooliveiraalice@hotmail.com>
2022-06-07 13:27:43 -0300

    tipos de dados primitivos

--- javascript/aula9/index.js
+++ javascript/aula9/index.js
@@ -1,4 +1,16 @@
-var nome = "luiz";
-var nome = "Otavio";
+// string, number, undefined, null, boolean, symbol
+const nome = "luiz"; // string
+const nome1 = "luiz"; // string
+const nome2 = "luiz"; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined = não aponta pra lugar nenhum na memória
+const sobrenomeAluno = null; // nulo > não aponta pra local nenhum na memória
+const aprovado = false; // boolean = true, false (logico)
 
-console.log(nome);
+let a = 2;
+const b = a;
+console.log(a, b); // 2, 2
+
+a = 3;
+console.log(a, b); // 3, 2

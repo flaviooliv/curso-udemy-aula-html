f77498a3ec94be641bf6bf8db0652a7f772bf287
flaviooliv <flaviooliveiraalice@hotmail.com>
2022-07-01 10:26:17 -0300

    Exercicios com Math e numbers

--- javascript/aula15/ex/js/script.js
+++ javascript/aula15/ex/js/script.js
@@ -2,5 +2,19 @@ const numero = Number(prompt("Digite um numero:"));
 const numeroTitulo = document.getElementById("numero-titulo");
 const texto = document.getElementById("texto");
 
+/*
+Raiz quadrada: 7.42958649595986
+55.1987555 é inteiro: false
+arredondando para baixo: 55
+arredondando para cima: 56
+com duas casas decimais: 55.20
+*/
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>Seu número + 2 é ${numero + 2}.</p>`;
+
+texto.innerHTML = "";
+texto.innerHTML += `<p>Raiz quadrada:${numero ** 0.5}.</p>`;
+texto.innerHTML += `<p>É NaN:${Number.isNaN(numero)}.</p>`;
+texto.innerHTML += `<p>${numero} é inteiro:${Number.isInteger(numero)}.</p>`;
+texto.innerHTML += `<p>arredondando para baixo: ${Math.floor(numero)}.</p>`;
+texto.innerHTML += `<p>arredondando para cima: ${Math.ceil(numero)}.</p>`;
+texto.innerHTML += `<p>com duas casas decimais: ${numero.toFixed(2)}.</p>`;

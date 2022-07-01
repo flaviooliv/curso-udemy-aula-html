a3224a85ffbfd91baa86c8fac0db4451a308a02c
flaviooliv <flaviooliveiraalice@hotmail.com>
2022-07-01 09:59:51 -0300

    Exercicios com numbers e math

--- javascript/aula15/index.js
+++ javascript/aula15/index.js
@@ -4,4 +4,4 @@ let num1 = 9.54578;
 // let num2 = Math.round(num1)
 //console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
 //console.log(Math.mix(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
-const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);

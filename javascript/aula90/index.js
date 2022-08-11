39fc9822a862e580c43a5262040674032da25581
flaviooliv <flaviooliveiraalice@hotmail.com>
2022-08-11 10:56:28 -0300

    fetch api

--- javascript/aula90/index.js
+++ javascript/aula90/index.js
@@ -2,3 +2,8 @@ const request = (obj) => {
   const xhr = new XMLHttpRequest();
   xhr.open(obj.method, obj.url, true);
 };
+fetch("pagina1.html")
+  .then((resposta) => {
+    if (resposta.status != 200) throw new Error("ERRO 404 MOSSO");
+  })
+  .catch((e) => console.log(e));
